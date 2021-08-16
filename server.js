75c6cdc2984e9c663567b0c7a4764482d471f580
root <root@LTUCSTUDENT-L154.localdomain>
2021-08-16 18:37:29 +0300

    add api

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+'use strict';
 
+const express = require('express');
+
+const server = express();
+
+const PORT = 3001;
+
+
+//localhost:3001/test
+server.get('/test',(request,response)=>{
+    response.send('your server is working')
+})
+
+server.listen(PORT,()=>{
+    console.log(`Listning on PORT ${PORT}`)
+})
 
 
